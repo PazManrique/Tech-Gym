630649c9871ff40357280f1ed69ad585b6f44b54
jilbosch <jilbosch@icloud.com>
2022-07-21 11:17:11 +0200

    #2 Home hecho

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import ProductDetail from "../Product-detail/Product-detail";
+import Home from "../pages/Home";
 
 
 export default function Router() {
     return(
         <Routes>
             <Route path= "/Catalogo" exact element ={<ProductDetail />}/>
+            <Route path= "/" exact element ={<Home/>}/>
         </Routes>
     )
 }
