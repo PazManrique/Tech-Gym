27da55d9d31a56b38f493d53f355e36a8940270e
jilbosch <jilbosch@icloud.com>
2022-07-21 14:03:57 +0200

    merge

---
+++ src/components/styles/Home.styled.jsx
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+
+export const HomeContainerStyle = styled.section`
+   
+`
+export const Categoria = styled.section `
+background-color: red;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  position: relative;
+width: 30em;
+height: 15em;
+&:hover{
+
+background-color: blue;
+
+
+}
+
+`;
+export const ContainerCategoria = styled.div `
+    justify-content: space-between;
+    text-align: center;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    color:#f5be30;
+
+   
+    
+`;
+
+
+export const TextCategoria = styled.h3 `
+    color:#f5be30;
+    
+
+`
